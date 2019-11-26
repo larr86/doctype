6756c725c396b761240a8766c9dd351334689807
larr86 <luisadrian.rr@gmail.com>
2019-11-25 22:32:52 -0600

    Inicio

---
+++ script.js
@@ -0,0 +1,21 @@
+// function validateForm(){
+//     //valido el email
+//     if (document.formulario.email.value.length==0){
+//            alert("Tiene que escribir su email")
+//            document.fvalida.email.focus()
+//            return 0;
+//     }
+ 
+//     //valido el password
+//     if (document.formulario.passInput.selectedIndex==0){
+//            alert("Debe escribir su contraseña.")
+//            document.formulario.passInput.focus()
+//            return 0;
+//     }
+ 
+//     //el formulario se envia
+//     alert("Muchas gracias por enviar el formulario");
+//     document.formulario.submit();
+// }
+
+
